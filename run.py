8125913f195d17c9de172f2ac48937cac8029678
Kamal-Kohli <>
2023-08-13 17:37:28 +0000

    Add commets to game

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
+"""
+Legend:
+1. "." = Water or empty space.
+2. "s" = Ships, Ship positions.
+3. "#" = Water that was shot with bullets, a miss it hit no ship.
+4. "x" = Ship Hit!
+
+Battleship:
+A. grid = 10x10Custom grids between min 5 to max 10.
+B. 5 ships of variable length randomly placed.
+C. Every hit and misses shot will be show up in the grid.
+D. If all ships are unearthed before using up all bullets, 
+    You Win else, You lose.
+E. You can choose a row and column such as A1, B1 to indicate where to shoot.
+"""
+
+# Constants and globals
+EMPTY = ''' . '''
+SHIP = ''' S '''
+HIT = ''' X  '''
+MISS = ''' # '''
+GRID_SIZE = ''' Between 5 to 10 '''  
+SHIPS = [5, 4, 3, 2, 2]  # Variation sizes of ships
+NUM_SHIPS = 5
+
+
 import random
 
 # Function to initialize the game boards
@@ -130,3 +156,4 @@ if __name__ == "__main__":
                 break
         else:
             print("Invalid grid size. Please enter a value between 5 and 10.")
+    main()
