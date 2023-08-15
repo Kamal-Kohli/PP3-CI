c10ca6f828675e2d7dd546ab70c00ecfc159d68d
Kamal-Kohli <>
2023-08-15 15:17:28 +0000

    reduce code length line 52 in print boards

--- run.py
+++ run.py
@@ -9,8 +9,8 @@ Battleship:
 A. grid = 10x10Custom grids between min 5 to max 10.
 B. 5 ships of variable length were randomly placed.
 C. Every hit or miss shot will show up in the grid.
-D. If all ships are unearthed before using up all bullets, 
-    You Win else, You lose.
+D. If all ships are unearthed before using up all bullets,
+You Win else, You lose.
 E. You can choose a row and column such as A1, B1 to indicate where to shoot.
 """
 
@@ -33,7 +33,7 @@ Now you're ready for the Navel war Captain. GOOD LUCK!
 SHIP = ''' S '''
 HIT = ''' X  '''
 MISS = ''' # '''
-GRID_SIZE = ''' Between 5 to 10 '''  
+GRID_SIZE = ''' Between 5 to 10 '''
 SHIPS = [5, 4, 3, 2, 2]  # Variation sizes of ships
 NUM_SHIPS = 5"""
 
@@ -49,9 +49,12 @@ def print_boards(player, computer, size, hide_comp=True):
     print(f"{username} Board:      CPU Board:")
     print("   " + col_labels + "   " + "   " + col_labels)
     for i in range(size):
-        comp_row = ["." if c == 'S' else c for c in computer[i]] if hide_comp else computer[i]
+        comp_row = [
+            '.' if c == 'S' else c for c in computer[i]
+        ] if hide_comp else computer[i]
         row_str = f"{i}  {' '.join(player[i])}   {i}  {' '.join(comp_row)}"
         print(row_str)
+
 
 # Function to place ships on the board
 def place_ship(board, ship_size):
@@ -157,7 +160,7 @@ def play_battleship(size, uname):
             print("Thank you for playing!")
 
 
-iif __name__ == "__main__":
+if __name__ == "__main__":
     while True:
         size = int(input("Enter grid size (5-10): "))
         if 5 <= size <= 10:
@@ -169,5 +172,3 @@ iif __name__ == "__main__":
                 break
         else:
             print("Invalid grid size. Enter a value between 5 and 10.")
-
-   
